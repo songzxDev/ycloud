f65c2120e219eb860dc89bcdbe21d81179488ea1
lovezjj008 <1318194191@qq.com>
2017-10-27 15:15:19 +0800

    switch 不可用状态
    Signed-off-by: lovezjj008 <1318194191@qq.com>

--- src/components/switch/index.js
+++ src/components/switch/index.js
@@ -3,6 +3,7 @@
 * description: switch 组件
 * 参数说明:
 * @checked: ko对象 Boolean 类型
+* @disabled: ko对象 Boolean 类型
 * @size: String类型:large, small (大, 小)
 * */
 import template from './index.html'
@@ -10,13 +11,18 @@ import ko from 'knockout'
 function init (params) {
   var self = this
   const PREFIX = 'y-switch-' // 通用前缀
+  this.disabled = params.disabled ? params.disabled : ko.observable(false)
   this.checked = params.checked || ko.observable(false) // 请传入ko对象 Boolean 值
   this.size = params.size ? (PREFIX + params.size) : '' // 尺寸  String 类型
   this.sizeClass = ko.computed(() => {
-    return self.checked() ? `${this.size} ${PREFIX}checked` : `${this.size} ${PREFIX}`
+    return self.disabled() ? self.checked() ? `${this.size} ${PREFIX}checked ${PREFIX}disabled` : `${this.size} ${PREFIX}disabled` : self.checked() ? `${this.size} ${PREFIX}checked` : `${this.size} ${PREFIX}`
   }, this)
   this.checkSelect = function () {
-    this.checked() ? this.checked(false) : this.checked(true)
+    if (self.disabled()) {
+      return false
+    } else {
+      this.checked() ? this.checked(false) : this.checked(true)
+    }
   }
 }
 export default {
