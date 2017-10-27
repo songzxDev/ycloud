dd2adbe55851ed02a6c8fa921044aeb19c21eef2
lovezjj008 <1318194191@qq.com>
2017-10-27 14:34:16 +0800

    switch 开关 Signed-off-by: lovezjj008 <1318194191@qq.com>

--- src/components/switch/index.js
+++ src/components/switch/index.js
@@ -1,14 +1,23 @@
+/*
+* author: ZJJ
+* description: switch 组件
+* 参数说明:
+* @checked: ko对象 Boolean 类型
+* @size: String类型:large, small (大, 小)
+* */
 import template from './index.html'
 import ko from 'knockout'
-import './switch.css'
 function init (params) {
-  this.checked = params.checked || ko.observable(false)
-  this.callback = params.callback || false
-  this.checked.subscribe(val => {
-    if (this.callback) {
-      this.callback(val)
-    }
-  })
+  var self = this
+  const PREFIX = 'y-switch-' // 通用前缀
+  this.checked = params.checked || ko.observable(false) // 请传入ko对象 Boolean 值
+  this.size = params.size ? (PREFIX + params.size) : '' // 尺寸  String 类型
+  this.sizeClass = ko.computed(() => {
+    return self.checked() ? `${this.size} ${PREFIX}checked` : `${this.size} ${PREFIX}`
+  }, this)
+  this.checkSelect = function () {
+    this.checked() ? this.checked(false) : this.checked(true)
+  }
 }
 export default {
   name: 'switch',
