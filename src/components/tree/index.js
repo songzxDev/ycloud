38cd9fbd6e3158e4027ef40b368545e297204522
windknow <songhlc@yonyou.com>
2017-10-28 09:27:17 +0800

    添加树组件 添加异步树组件

--- src/components/tree/index.js
+++ src/components/tree/index.js
@@ -8,7 +8,14 @@ ko.components.register(PREFIX + treenode.name, {
 })
 function init (params) {
   this.data = params.data
+  this.height = params.height || 'auto'
   this.loadData = params.loadData
+  this.selectedItem = params.selectedItem || ko.observable({})
+  this.selectedItem.subscribe(val => {
+    if (params.onSelect) {
+      params.onSelect(val)
+    }
+  })
 }
 
 export default {
