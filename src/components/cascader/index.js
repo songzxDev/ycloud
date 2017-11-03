6b39000aa6f899071bb2d6b99d96d73145048cdf
liuxu7 <liuxu7@ufsoft.com.cn>
2017-11-03 18:50:45 +0800

    fix: cascader展开把下面遮住了

--- src/components/cascader/index.js
+++ src/components/cascader/index.js
@@ -11,7 +11,17 @@ import ko from 'knockout'
 function init (params) {
   this.data = params.data
   this.selectedValue = params.selectedValue
+  this.hidden = ko.observable(false)
   this.casitmevisible = ko.observable(false)
+  this.casitmevisible.subscribe((val) => {
+    if (val) {
+      this.hidden(!val)
+    } else {
+      setTimeout(() => {
+        this.hidden(!val)
+      }, 500)
+    }
+  })
   this.clearable = params.clearable || false
   // 用于判断是否显示关闭按钮
   this.showCloseIcon = ko.computed(() => {
