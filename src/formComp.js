53f80ca2e58c08a0c877ef570c611fcc6e2078b6
windknow <songhlc@yonyou.com>
2018-01-17 16:59:10 +0800

    运行错误

---
+++ src/formComp.js
@@ -0,0 +1,40 @@
+import ko from 'knockout'
+var template = `
+  <div data-bind="let:{model2: row}">
+    <y-form params="labelWidth:'120px',labelAlign:'right'">
+      <div class="col-md-3">
+      <y-formitem params="label:'id:'">
+        <y-input params="value: model2.id"></y-input>
+      </y-formitem>
+      </div>
+      <div class="col-md-3">
+      <y-formitem params="label:'名称:'">
+        <y-input params="value: model2.name"></y-input>
+      </y-formitem>
+      </div>
+      <div class="col-md-offset-6"></div>
+      <div class="col-md-3">
+      <y-formitem params="label:'可选择标签:'">
+        <y-searchtag params="enableActive:true,tagList: $root.tagList"></y-searchtag>
+      </y-formitem>
+      </div>
+      <div class="col-md-offset-6"></div>
+    </y-form>
+    <div class="col-md-12">
+      <y-button params="click:handleSave">保存</y-button>
+    </div>
+  </div>
+`
+var init = function (params) {
+  this.row = params.value
+  this.handleSave = function () {
+    console.log(this.row.id)
+    console.log(this.row.name)
+    console.log(this.row)
+    this.row._expand(!this.row._expand())
+  }.bind(this)
+}
+ko.components.register('demo-form', {
+  viewModel: init,
+  template: template
+})
