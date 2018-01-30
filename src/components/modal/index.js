b58f5722e889f90455d8133f20a4eb54540bc735
windknow <songhlc@yonyou.com>
2018-01-30 14:51:07 +0800

    1.4.10 y-modal add parameter lazy

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -7,6 +7,7 @@ function init (params) {
   this.isShow = ko.observable(false)
   this.width = params.width || '400px'
   this.title = params.title || '提示'
+  this.isNotLazy = !params.lazy
   this.top = window.innerHeight * 0.06 + 'px'
   var headAndFootHeightRate = (43 + 57) / window.innerHeight
   // 容器最大高度等于 屏幕可视高度 - 头部 - 尾部 - 内存padding - 距离顶部距离10% - 距离底部最小距离10%
@@ -42,11 +43,16 @@ function init (params) {
     }
   })
   // 仅当visible第一次是true的时候切换if
-  ko.when(() => {
-    return this.visible()
-  }, () => {
+  if (this.isNotLazy) {
     this.isShow(true)
-  }, this)
+  } else {
+    ko.when(() => {
+      return this.visible()
+    }, () => {
+      this.isShow(true)
+    }, this)
+  }
+
   // 添加外层错误校验
   this.errormsg = ko.observable('')
   this.isValidate = true
