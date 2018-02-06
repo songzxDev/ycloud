7637dcbd9bef7f5d12d9c40c250e871a9e1dbd7f
wangk2 <Administrator@uf201202336.ufsoft.com.cn>
2018-02-06 16:25:19 +0800

    面包屑

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -9,6 +9,8 @@ import template from './index.html'
 function init (params) {
   this.data = params.data
   this.separator = params.separator || '>'
+  this.lastRow = this.data[this.data.length - 1]
+  this.exceptLastRowArr = this.data.slice(0, this.data.length - 1)
 }
 export default {
   name: 'breadcrumb',
