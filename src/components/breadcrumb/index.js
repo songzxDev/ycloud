99de5ee5a1eb07390df528f4f0fec14fff726ee4
wangk2 <Administrator@uf201202336.ufsoft.com.cn>
2018-02-06 17:15:31 +0800

    面包屑

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -5,12 +5,18 @@
 *
 * */
 import template from './index.html'
+import ko from 'knockout'
 
 function init (params) {
   this.data = params.data
   this.separator = params.separator || '>'
-  this.lastRow = this.data[this.data.length - 1]
-  this.exceptLastRowArr = this.data.slice(0, this.data.length - 1)
+  if (ko.isObservable(this.data)) {
+    this.lastRow = this.data()[this.data().length - 1]
+    this.exceptLastRowArr = this.data().slice(0, this.data.length - 1)
+  } else {
+    this.lastRow = this.data[this.data.length - 1]
+    this.exceptLastRowArr = this.data.slice(0, this.data.length - 1)
+  }
 }
 export default {
   name: 'breadcrumb',
