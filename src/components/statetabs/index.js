9eff542edec542c0c3a2145366582ea5b502040e
windknow <songhlc@yonyou.com>
2018-05-03 15:36:44 +0800

    statetabs使用新的动画效果重构

--- src/components/statetabs/index.js
+++ src/components/statetabs/index.js
@@ -26,20 +26,13 @@ class StateTab extends Base {
       let tabList = el.querySelectorAll('.y-state-tab')
       let style = {}
       let contentWidth = 0
-      let contentHeight = 0
       let transformLeft = 0
-      tabList.forEach((node, index) => {
-        // 计算当前节点之前的宽度
-        if (index < curIndex) {
-          transformLeft += node.offsetWidth
-        }
-        // 计算当前节点的高度和宽度
-        if (index === curIndex) {
-          contentWidth = node.offsetWidth
-          contentHeight = node.offsetHeight
-        }
-      })
-      style.transform = `translateX(${transformLeft}px) translateY(${contentHeight}px) translateZ(0px)`
+      if (curIndex >= 0 && curIndex < tabList.length) {
+        let curNode = tabList[curIndex]
+        transformLeft = curNode.offsetLeft
+        contentWidth = curNode.offsetWidth
+      }
+      style.transform = `translateX(${transformLeft}px) translateZ(0px)`
       style.width = contentWidth + 'px'
       this.stateActiveStyle(style)
     }
