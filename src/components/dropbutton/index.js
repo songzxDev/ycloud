9879926537969b0c6d1c2ed3b2e29aa22c92265a
wjk1993 <wangjk9@yonyou.com>
2018-07-31 20:29:18 +0800

    dropbutton下拉按钮222

--- src/components/dropbutton/index.js
+++ src/components/dropbutton/index.js
@@ -1,21 +1,25 @@
+// click 参数是 主点击事件
+// dropList 下拉按钮组的data  类似{value:1，label:'新增'}
+// onDropList 下拉按钮组点击事件 根据参数判断执行事件
+// 欢迎吐槽  by wjk
 import template from './index.html'
 import ko from 'knockout'
 import './index.less'
 function init (params) {
-  this.mainText = params.mainText || '新增'
-  this.mainClick = params.mainClick || function () {}
+  this.text = params.text || '新增'
+  this.mainClick = params.click || function () {}
   this.isShowDrop = ko.observable(false)
   this.showdropdown = () => {
     this.isShowDrop(!this.isShowDrop())
   }
-  this.dropWidth = params.dropWidth || 85
-  this.dataList = params.dataList || ko.isObservableArray([])
+  this.dropWidth = params.dropWidth
+  this.dropList = params.dropList || ko.isObservableArray([])
   this.handleOptClick = function (item) {
-    params.handleOptClick(item)
+    params.onDropList(item)
   }
 }
 export default {
-  name: 'dropbutton',
+  name: 'button-group',
   init,
   template
 }
